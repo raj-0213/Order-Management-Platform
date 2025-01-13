52f451848bdf1571cbc19efc23351a6e3aa9dfe8
raj-0213 <raj.f@medkart.in>
2025-01-13 10:11:39 +0530

    Created APIs

--- backEnd/server.js
+++ backEnd/server.js
@@ -1,23 +1,41 @@
 const express = require("express");
 const http = require("http");
-// require("dotenv").config({ path: "./.env" });
+const db = require("./database/config");
+const { sequelize } = require('./models'); 
+// console.log("Sequelize instance:", sequelize);
+
+const {productRoutes} = require("./routes");
+const {cartRoutes} = require("./routes");
+const {orderRoutes} = require("./routes");
+const {userRoutes} = require("./routes");
+
+
+require("dotenv").config();
 
 const app = express();
 const PORT = process.env.PORT || 5000;
 const HOST = process.env.HOST || "localhost"; 
 
-// Middleware to parse JSON
 app.use(express.json({ limit: "30mb", extended: true }));
 app.use(express.urlencoded({ limit: "30mb", extended: false }));
 
-// Simple route to verify server is running
+app.use("/product",productRoutes);
+app.use("/cart",cartRoutes);
+app.use("/order",orderRoutes);
+app.use("/user",userRoutes);
+
+
 app.get("/", (req, res) => {
   res.send("Server is running");
 });
 
-// Create the server and start listening
+sequelize.sync({ force: false }).then(() => {
+  console.log("Database Synced");
+});
+
 const server = http.createServer(app);
 
 server.listen(PORT, () => {
   console.log(`Server listening on http://${HOST}:${PORT}`);
 });
+
